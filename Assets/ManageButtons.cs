efff524766c0cc4d304a92ca9da9272d05c58ecc
Chasejones5 <jones441@nsuok.edu>
2024-04-09 15:07:17 -0500

    First Commit

---
+++ Assets/ManageButtons.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "wordGameStart") PlayerPrefs.SetInt("score", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startWordGame()
+    {
+        SceneManager.LoadScene("wordGame");
+    }
+    public void LoadStartWordGame()
+    {
+        SceneManager.LoadScene("wordGameStart");
+    }
+}
